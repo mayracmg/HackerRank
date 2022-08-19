39cf611558347546db41c36ed144d66e10e66f0f
Mayra Morataya <70965559+mayracmg@users.noreply.github.com>
2022-08-18 20:46:40 -0600

    Tower Breakers
    Tenemos 2 jugadores, el jugador 1 siempre hacer el primer movimiento y los dos jugadores siempre juegan de forma óptima.

---
+++ tower_breakers.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'towerBreakers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def towerBreakers(n, m):
+    winner = 0
+    
+    if (m == 1):
+        winner = 2
+    else:
+        if ((n % 2) == 0):
+            winner = 2
+        else:
+            winner = 1
+            
+    return winner
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        result = towerBreakers(n, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
