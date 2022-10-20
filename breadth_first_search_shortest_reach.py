a771a458f0a7fc08b50916185ec2b1061cfca821
Mayra Morataya <70965559+mayracmg@users.noreply.github.com>
2022-10-20 12:25:48 -0600

    Breadth First Search: Shortest Reach
    Consider an undirected graph where each edge weighs 6 units. Each of the nodes is labeled consecutively from 1 to n.
    
    You will be given a number of queries. For each query, you will be given a list of edges describing an undirected graph. After you create a representation of the graph, you must determine and report the shortest distance to each of the other nodes from a given starting position using the breadth-first search algorithm (BFS). Return an array of distances from the start node in node number order. If a node is unreachable, return -1 for that node.

---
+++ breadth_first_search_shortest_reach.py
@@ -0,0 +1,77 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import queue
+
+def transform_object(nodes, s):
+    distances = []
+    for key, node in nodes.items():
+        distance = node[0]
+        if (distance == 0):
+            distance = -1
+        if (key != s):
+            distances.append(distance)
+    return distances
+
+def validate_completion(nodes, q, e, edge_start, edge_end):
+    if (nodes[edge_end][1] == False):
+        nodes[edge_end][0] = 6 + nodes[edge_start][0]
+        
+        if (nodes[edge_start][1] == True):
+            nodes[edge_end][1] = True
+            nodes[edge_start][1] = True
+
+        q.put(edge_end)
+        edges.remove(e)
+    
+def bfs(n, m, edges, s):
+    nodes = {key: [0, False] for key in range(1, n + 1)}
+
+    q = queue.Queue()
+    q.put(s)
+    nodes[s][1] = True
+        
+    while not q.empty():
+        start = q.get()
+        adjacentEdges = list(filter(lambda e: start in e, edges))
+            
+        for e in adjacentEdges:
+            edge_start = e[0]
+            edge_end = e[1]
+            
+            validate_completion(nodes, q, e, edge_start, edge_end)
+            validate_completion(nodes, q, e, edge_end, edge_start)
+
+    distances = transform_object(nodes, s)
+        
+    return distances            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        edges = []
+
+        for _ in range(m):
+            edges.append(list(map(int, input().rstrip().split())))
+
+        s = int(input().strip())
+
+        result = bfs(n, m, edges, s)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
