dfc81cbe582f6455fa778581f27981889940e2d6
Mayra Morataya <70965559+mayracmg@users.noreply.github.com>
2022-11-05 21:32:39 -0600

    Divisible Sum Pairs
    Tenemos array de números enteros.
    Y un  número K.
    Debemos encontrar parejas de números que la suma sea divisible entre K.

---
+++ divisible_sum_pairs.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar):
+    pairs = 0
+    
+    for i in range(n):
+        for j in range(i + 1, n):
+            number_1 = ar[i]
+            number_2 = ar[j]
+            pair = number_1 + number_2
+            
+            if (pair % k == 0):
+                pairs += 1
+                
+    return pairs    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
