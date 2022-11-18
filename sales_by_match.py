56177e8b15221ccc669fe9c26a73dc466e5c0025
Mayra Morataya <70965559+mayracmg@users.noreply.github.com>
2022-11-17 20:35:23 -0600

    Sales by Match
    Encuentra el numero de pares de calcetines.

---
+++ sales_by_match.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    total_pairs = 0
+    pairs = {}
+    for sock in ar:
+        try:
+            pairs[sock] += 1
+            if (pairs[sock] % 2 == 0):
+                total_pairs += 1
+        except:
+            pairs[sock] = 1
+
+    return total_pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
